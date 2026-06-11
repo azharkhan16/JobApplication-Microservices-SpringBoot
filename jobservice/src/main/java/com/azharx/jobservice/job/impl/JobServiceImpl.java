badbaa830b24a6035405b8fb5a9237c3b4a4c0f6
azharkhan16 <azharkhan75447355@gmail.com>
2026-06-11 05:51:29 +0530

    feat: implement CRUD operations for jobservice

---
+++ jobservice/src/main/java/com/azharx/jobservice/job/impl/JobServiceImpl.java
@@ -0,0 +1,62 @@
+package com.azharx.jobservice.job.impl;
+import com.azharx.jobservice.job.Job;
+import com.azharx.jobservice.job.JobRepository;
+import com.azharx.jobservice.job.JobService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JobServiceImpl implements JobService {
+
+    private JobRepository jobRepository;
+
+    public JobServiceImpl(JobRepository jobRepository) {
+        this.jobRepository = jobRepository;
+    }
+
+
+    @Override
+    public List<Job> findAll() {
+        return jobRepository.findAll();
+    }
+
+    @Override
+    public void createJob(Job job) {
+        jobRepository.save(job);
+    }
+
+    @Override
+    public Job getJobById(Long id) {
+        return jobRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public boolean deleteJobById(Long id) {
+        try {
+            jobRepository.deleteById(id);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean updateJobById(Long id, Job updatedJob) {
+        Optional<Job> jobOptional = jobRepository.findById(id);
+        if (jobOptional.isPresent()) {
+            Job job = jobOptional.get();
+            job.setTitle(updatedJob.getTitle());
+            job.setDescription(updatedJob.getDescription());
+            job.setMinSalary(updatedJob.getMinSalary());
+            job.setMaxSalary(updatedJob.getMaxSalary());
+            job.setLocation(updatedJob.getLocation());
+            jobRepository.save(job);
+            return true;
+        }
+        return false;
+    }
+
+
+}
